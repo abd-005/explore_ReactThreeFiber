497074a9dea9b57af7ea8c9486d067b375452d64
Md Abdul Ahad <abd.me7@gmail.com>
2026-05-18 20:05:07 +0600

    ambientLight group Implemented

--- src/App.jsx
+++ src/App.jsx
@@ -15,15 +15,20 @@ const App = () => {
 
   return (
     <Canvas>
-      <directionalLight position={[1, 1, 3]} />
+      {/* Lighting */}
+      <directionalLight position={[1, 1, 3]} intensity={0.5}/>
+      <ambientLight intensity={0.1}/>
 
+      {/* Materials */}
+      <group position={[0, 0, 1]}>  
       <Cube position={[1, 1, 0]} color={"lightgreen"} size={[1, 1, 1]} />
 
       <Cube position={[-1, -1, 0]} color={"lightblue"} size={[1, 1, 1]} />
 
       <Cube position={[-1, 1, 0]} color={"cyan"} size={[1, 1, 1]} />
-      
+
       <Cube position={[1, -1, 0]} color={"tomato"} size={[1, 1, 1]} />
+      </group>
     </Canvas>
   )
 }
